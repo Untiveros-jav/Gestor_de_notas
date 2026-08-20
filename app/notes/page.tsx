80c839e845a77824c7a0464b7e886fad18ce0bc3
Luis Alberto <untiveros2024@gmail.com>
2026-08-20 00:23:13 -0500

    Commit 5: Se procedio con la creacion del archivo noteList encargado de filtrar las notes importantes

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link"
 import { getNotes } from "../services/notes"
+import NoteList from "./notelist/page"
 
 
 
@@ -8,16 +8,7 @@ const Notes = () => {
     return (
         <div>
             <h2>Notes</h2>
-            <ul>
-                {
-                    notes.map(note => (
-                        <li key={note.id}>
-                            <Link href={`/notes/${note.id}`}>{note.content}</Link>
-                            {note.important && <strong>(important)</strong>} 
-                        </li>
-                    ))
-                }
-            </ul>
+            <NoteList notes={notes} />
         </div>
     )
 }
